be13dab25ea279b54aa63e4b9e3f83e27ec5230d
Wapeto <dupetopetoman@gmail.com>
2024-06-06 16:44:32 +0200

    Add vercel speedInsight and Analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from '@vercel/speed-insights/react';
 import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import ContactPage from "./pages/ContactPage.tsx";
 
@@ -18,5 +20,7 @@ const router = createBrowserRouter([
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <RouterProvider router={router} />
+        <Analytics />
+        <SpeedInsights />
     </React.StrictMode>,
 );
